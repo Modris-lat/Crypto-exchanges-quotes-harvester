510f1d6c06eec8f230c3ac1be392e77d950d636e
Modris-lat <belkovskis.modris@gmail.com>
2020-11-08 17:19:56 +0200

    removed previous changes in binance, starting to work on ploniex client

--- Crypto-exchange-quotes-harvester/ConsoleApp/Program.cs
+++ Crypto-exchange-quotes-harvester/ConsoleApp/Program.cs
@@ -17,21 +17,31 @@ namespace ConsoleApp
         {
             IAccessKeys keys = new AccessKeys();
             IApiClient api = new ApiClient(keys.ApiKey, keys.ApiSecretKey);
-            IBinanceMarketInfo getBinanceMarketInfo = new BinanceMarketInfo(new BinanceClient(api));
-            var streamInfo = await getBinanceMarketInfo.BinanceClient.StartUserStream();
+            IBinanceClient binanceClient = new BinanceClient(api);
+            var streamInfo = await binanceClient.StartUserStream();
             var listenKey = streamInfo.ListenKey;
             bool loop = true;
             while (loop)
             {
-                await getBinanceMarketInfo.GetBinanceMarketInfo();
+                var binanceMarketInfo = await binanceClient.GetOrderBookTicker();
+                var currencyOrderList = binanceMarketInfo.ToList();
+                for (int i = 0; i < currencyOrderList.Count; i++)
+                {
+                    if (currencyOrderList[i].Symbol == "ETHBTC")
+                    {
+                        Console.WriteLine(currencyOrderList[i].Symbol);
+                        Console.WriteLine(currencyOrderList[i].AskPrice);
+                        Console.WriteLine(currencyOrderList[i].BidPrice);
+                    }
+                }
                 var input = Console.ReadKey().KeyChar;
-                if (input == 'a')
+                if (input == 'q')
                 {
                     loop = false;
                 }
             }
 
-            await getBinanceMarketInfo.BinanceClient.CloseUserStream(listenKey);
+            await binanceClient.CloseUserStream(listenKey);
         }
     }
 }
---
+++ Crypto-exchange-quotes-harvester/Poloniex.API.Client/IPoloniexClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Modules.Market;
+
+namespace Poloniex.API.Client
+{
+    public interface IPoloniexClient
+    {
+        Task<dynamic> TestConnectivity();
+        Task<IEnumerable<OrderBookTicker>> GetOrderBookTicker();
+    }
+}
